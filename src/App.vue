86321e204cf4e376bcd87cbdeaf325ed4bc5dfc4
agent_tim <agent_tim@mail.ru>
2023-04-14 17:36:53 +0500

    2023_04_14

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    <form class="card">
+      <h2>Работа с базой данных</h2>
+
+      <div class="form-control">
+        <label for="name">Введите имя</label>
+        <input type="text" id="name" v-model.trim="name">
+      </div>
+
+      <button class="btn primary">Создать человека</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import './main/resources/vue/style/theme.css'
+
+createApp(App).mount('#app')
+
