50d179fe77715de35ddc27fdb5af6f555b04f230
agent_tim <agent_tim@mail.ru>
2023-04-18 20:35:36 +0500

    2023_04_18_2

--- src/App.vue
+++ src/App.vue
@@ -1,65 +1,34 @@
 <template>
   <div class="container">
-    <form class="card" @submit.prevent="createPerson">
-      <h2>Работа с базой данных</h2>
-
-      <div class="form-control">
-        <label for="name">Введите имя</label>
-        <input type="text" id="name" v-model.trim="name">
-      </div>
-
-      <button class="btn primary" :disabled="name.length === 0">Создать человека</button>
-    </form>
-
-    <app-people-list
-        :people="people"
-        @load="loadPeople"
-    ></app-people-list>
+    <div class="card">
+      {{ info }}
+    </div>
   </div>
 </template>
 
 <script>
-import AppPeopleList from "@/main/resources/vue/component/AppPeopleList";
-
+//import axios from 'axios'
 export default {
   data() {
     return {
-      name: '',
-      people: []
-    }
+      info: null
+    };
   },
   methods: {
-/*    async loadPeople() {
-      let response = await fetch('http://localhost:8081/people', { mode: 'no-cors'})
-      console.log(response)
-    }*/
     loadPeople() {
-      fetch('http://localhost:8081/people',{ mode: 'no-cors'})
-          .then((response) => {
-            return response.json();
-          })
-          .then((data) => {
-            console.log(data);
-          });
+      fetch('http://localhost:8081/people', {mode: "cors"})
+          //.then(res => console.log(res))
+          .then(res => res.json())
+          .then(res => res.map(user => user.name))
+          .then((data) => this.info = data)
+          .then(userNames => console.log(userNames))
+          .catch(error => console.log(error))
+
     }
-
-/*    async createPerson() {
-      const response = await fetch('http://', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          firstName: this.name
-        })
-      })
-
-      const firebaseData = await response.json()
-
-      console.log(firebaseData)
-    }*/
   },
-  components: {AppPeopleList}
+  mounted() {
+    this.loadPeople()
+  }
 }
 </script>
 
